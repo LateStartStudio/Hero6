f36342bc96206797624380fdb9e3773f0250dc17
Per Olav Flaten <perolavflaten@live.no>
2016-11-07 02:39:44 +0100

    Added unit tests for WindowsDX and DesktopGL
    Added some simple unit tests for the application projects. I thought it would be practical to have some unit tests that are able to discover run time failures on CI build servers and such.

---
+++ src/Hero6.Tests/GameTests.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GameTests.cs" company="LateStartStudio">
+//   Copyright (C) LateStartStudio
+//   This file is subject to the terms and conditions of the MIT license specified
+//   in the file 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+// <summary>
+//   Defines the GameTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LateStartStudio.Hero6.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GameTests
+    {
+        private Game game;
+
+        [SetUp]
+        public void Init()
+        {
+            this.game = new Game();
+            this.game.RunOneFrame();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            if (this.game != null)
+            {
+                this.game.Dispose();
+            }
+
+            this.game = null;
+        }
+
+        [Test]
+        public void InitSuccessful()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void GameIsNotNull()
+        {
+            Assert.IsNotNull(this.game);
+        }
+
+        [Test]
+        public void GraphicsDeviceIsNotNull()
+        {
+            Assert.IsNotNull(this.game.GraphicsDevice);
+        }
+    }
+}
