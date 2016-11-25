LateStartStudio/Hero6

commit fe39350e37403bc3e78d23b23a166217ad2ccd95

Mouse cursor rendering correctly
The rendering of the mouse cursor is moved to the UI module SierraVga, and it should now render correctly in front of the text box element.

assets/Hero6.Content.mgcb
@@ -205,7 +205,13 @@
 /processorParam:TextureFormat=Color
 /build:Sprites/Characters/NPCs/Llewella/LlewellaWalkCenterDown.png
 
-#begin Sprites/GUIs/Cursors/Arrow.png
+#begin Gui/SierraVga/Segoe_UI_11.25_Regular.spritefont
+/importer:FontDescriptionImporter
+/processor:FontDescriptionProcessor
+/processorParam:TextureFormat=Compressed
+/build:Gui/SierraVga/Segoe_UI_11.25_Regular.spritefont
+
+#begin Gui/SierraVga/MouseCursors/Arrow.png
 /importer:TextureImporter
 /processor:TextureProcessor
 /processorParam:ColorKeyColor=255,0,255,255
@@ -215,11 +221,17 @@
 /processorParam:ResizeToPowerOfTwo=False
 /processorParam:MakeSquare=False
 /processorParam:TextureFormat=Color
-/build:Sprites/GUIs/Cursors/Arrow.png
+/build:Gui/SierraVga/MouseCursors/Arrow.png
 
-#begin Gui/SierraVga/Segoe_UI_11.25_Regular.spritefont
-/importer:FontDescriptionImporter
-/processor:FontDescriptionProcessor
-/processorParam:TextureFormat=Compressed
-/build:Gui/SierraVga/Segoe_UI_11.25_Regular.spritefont
+#begin Gui/SierraVga/MouseCursors/Wait.png
+/importer:TextureImporter
+/processor:TextureProcessor
+/processorParam:ColorKeyColor=255,0,255,255
+/processorParam:ColorKeyEnabled=True
+/processorParam:GenerateMipmaps=False
+/processorParam:PremultiplyAlpha=True
+/processorParam:ResizeToPowerOfTwo=False
+/processorParam:MakeSquare=False
+/processorParam:TextureFormat=Color
+/build:Gui/SierraVga/MouseCursors/Wait.png
 

src/Hero6.UserInterface.SierraVGA/Hero6.UserInterface.SierraVGA.csproj
@@ -45,6 +45,7 @@
     <Reference Include="WindowsBase" />
   </ItemGroup>
   <ItemGroup>
+    <Compile Include="MouseCursor.cs" />
     <Compile Include="ViewModel\RootViewModel.cs" />
     <Compile Include="ViewModel\TextBoxViewModel.cs" />
     <Compile Include="View\RootView.xaml.cs">

src/Hero6.UserInterface.SierraVGA/MouseCursor.cs
@@ -1,0 +1,119 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MouseCursor.cs" company="LateStartStudio">
+//   Copyright (C) LateStartStudio
+//   This file is subject to the terms and conditions of the MIT license specified
+//   in the file 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+// <summary>
+//   Defines the MouseCursor type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LateStartStudio.Hero6.UserInterface.SierraVga
+{
+    using System;
+    using AdventureGame.Engine.Graphics;
+    using AdventureGame.Game;
+    using EmptyKeys.UserInterface;
+    using EmptyKeys.UserInterface.Input;
+    using EmptyKeys.UserInterface.Media;
+    using EmptyKeys.UserInterface.Renderers;
+
+    public class MouseCursor : ICursorService, IGameLoop
+    {
+        private readonly object content;
+        private readonly int screenWidth;
+        private readonly int screenHeight;
+        private readonly Vector2 scale;
+
+        private PointF location;
+        private Size area;
+        private TextureBase current;
+        private TextureBase arrow;
+        private TextureBase wait;
+
+        public MouseCursor(object content, int screenWidth, int screenHeight, Vector2 scale)
+        {
+            this.content = content;
+            this.screenWidth = screenWidth;
+            this.screenHeight = screenHeight;
+            this.scale = scale;
+
+            this.location = new PointF(0, 0);
+            this.area = new Size();
+        }
+
+        private static InputDeviceBase Input
+        {
+            get { return Engine.Instance.InputDevice; }
+        }
+
+        private static Renderer Renderer
+        {
+            get { return Engine.Instance.Renderer; }
+        }
+
+        private TextureBase Current
+        {
+            get
+            {
+                return this.current;
+            }
+
+            set
+            {
+                this.current = value;
+                this.area = new Size(value.Width * this.scale.X, value.Height * this.scale.Y);
+            }
+        }
+
+        public void SetCursorType(CursorType cursorType)
+        {
+            switch (cursorType)
+            {
+                case CursorType.Arrow:
+                    this.Current = this.arrow;
+                    break;
+                case CursorType.Wait:
+                    this.Current = this.wait;
+                    break;
+                default:
+                    throw new NotSupportedException(
+                        string.Format(
+                            "Cursor {0} is not supported by the Hero6 UI module SierraVga",
+                            cursorType));
+            }
+        }
+
+        public void Load()
+        {
+            this.arrow = this.LoadTexture("MouseCursors/Arrow");
+            this.wait = this.LoadTexture("MouseCursors/Wait");
+
+            this.Current = this.arrow;
+        }
+
+        public void Unload()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TimeSpan totalTime, TimeSpan elapsedTime, bool isRunningSlowly)
+        {
+            this.location.X = Input.MouseState.NormalizedX * this.screenWidth * this.scale.X;
+            this.location.Y = Input.MouseState.NormalizedY * this.screenHeight * this.scale.Y;
+        }
+
+        public void Draw(TimeSpan totalTime, TimeSpan elapsedTime, bool isRunningSlowly)
+        {
+            Renderer.Begin();
+            Renderer.Draw(this.Current, this.location, this.area, ColorW.White, false);
+            Renderer.End();
+        }
+
+        private TextureBase LoadTexture(string id)
+        {
+            return Engine.Instance.AssetManager.LoadTexture(this.content, id);
+        }
+    }
+}

src/Hero6.UserInterface.SierraVGA/SierraVGAController.cs
@@ -16,7 +16,9 @@ namespace LateStartStudio.Hero6.UserInterface.SierraVga
     using AdventureGame.Engine.Graphics;
     using AdventureGame.UI;
     using EmptyKeys.UserInterface;
+    using EmptyKeys.UserInterface.Input;
     using EmptyKeys.UserInterface.Media;
+    using EmptyKeys.UserInterface.Mvvm;
     using View;
     using ViewModel;
     using AdventureGameEngine = AdventureGame.Engine.Engine;
@@ -24,6 +26,8 @@ namespace LateStartStudio.Hero6.UserInterface.SierraVga
 
     public class SierraVgaController : UserInterface
     {
+        private readonly MouseCursor mouseCursor;
+
         private RootView rootView;
         private RootViewModel rootViewModel;
         private FontBase defaultFont;
@@ -37,11 +41,17 @@ namespace LateStartStudio.Hero6.UserInterface.SierraVga
             : base(width, height, scale, adventureGameEngine, content)
         {
             this.Content.RootDirectory = "Content/Gui/SierraVga";
+            this.mouseCursor = new MouseCursor(
+                this.Content.NativeContentManager,
+                this.Width,
+                this.Height,
+                this.Scale);
+            ServiceManager.Instance.AddService<ICursorService>(this.mouseCursor);
         }
 
         public override void Load()
         {
-            this.defaultFont = LoadFont("Segoe_UI_11.25_Regular");
+            this.defaultFont = this.LoadFont("Segoe_UI_11.25_Regular");
 
             FontManager.DefaultFont = this.defaultFont;
 
@@ -55,6 +65,8 @@ namespace LateStartStudio.Hero6.UserInterface.SierraVga
             };
 
             this.rootView.MouseUp += (s, a) => this.rootViewModel.TextBox.Hide();
+
+            this.mouseCursor.Load();
         }
 
         public override void Unload()
@@ -63,6 +75,7 @@ namespace LateStartStudio.Hero6.UserInterface.SierraVga
 
         public override void Update(TimeSpan totalTime, TimeSpan elapsedTime, bool isRunningSlowly)
         {
+            this.mouseCursor.Update(totalTime, elapsedTime, isRunningSlowly);
             this.rootView.UpdateInput(elapsedTime.TotalMilliseconds);
             this.rootView.UpdateLayout(elapsedTime.TotalMilliseconds);
         }
@@ -70,6 +83,7 @@ namespace LateStartStudio.Hero6.UserInterface.SierraVga
         public override void Draw(TimeSpan totalTime, TimeSpan elapsedTime, bool isRunningSlowly)
         {
             this.rootView.Draw(elapsedTime.TotalMilliseconds);
+            this.mouseCursor.Draw(totalTime, elapsedTime, isRunningSlowly);
         }
 
         public override void ShowTextBox(string text)

assets/Sprites/GUIs/Cursors/Arrow.png
@@ -1,3 +1,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:656470d011a2d24bcc2e06ffe13bfeff7a6b90965028d094c3eb996cb15703f1
-size 326

src/Hero6/Input/InputHandler.cs
@@ -25,7 +25,7 @@ namespace LateStartStudio.Hero6.Input
         public InputHandler(Matrix scale, ContentManager content)
         {
             this.Keyboard = new KeyboardHandler();
-            this.Mouse = new MouseHandler(content);
+            this.Mouse = new MouseHandler();
             this.Touch = new TouchHandler();
             this.Scale = new Vector2(scale.M11, scale.M22);
         }

src/Hero6/Input/Mouse/MouseHandler.cs
@@ -13,22 +13,17 @@ namespace LateStartStudio.Hero6.Input.Mouse
 {
     using System;
     using Microsoft.Xna.Framework;
-    using Microsoft.Xna.Framework.Content;
     using Microsoft.Xna.Framework.Graphics;
     using Microsoft.Xna.Framework.Input;
 
     public class MouseHandler : IXnaGameLoop
     {
-        private readonly ContentManager content;
-
         private MouseState previousState;
         private MouseState currentState;
         private Point position;
-        private Texture2D texture;
 
-        public MouseHandler(ContentManager content)
+        public MouseHandler()
         {
-            this.content = content;
             this.position = Vector2.Zero.ToPoint();
             this.Scale = Vector2.One;
         }
@@ -46,7 +41,6 @@ namespace LateStartStudio.Hero6.Input.Mouse
 
         public void Load()
         {
-            this.texture = this.content.Load<Texture2D>("Sprites/GUIs/Cursors/Arrow");
         }
 
         public void Unload()
@@ -79,7 +73,6 @@ namespace LateStartStudio.Hero6.Input.Mouse
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(this.texture, this.position.ToVector2());
         }
 
         private void InvokeMouseButtonUp(MouseButton mouseButton)

commit e7efa69dd45d3d89066e372b76c6ae82fbf3ecde

Fixed critical error
Commit fe39350e37403bc3e78d23b23a166217ad2ccd95 introduced a severe error of missing files that will block anyone from building the project. This commit should remedy that.

I also simplified the code referencing the directories to the assets, so that we could reference them simply by filename instead of entire directory path.

Finally I removed the gitignore for the files that are auto generated by EmptyKeys, as apparantly the generator program only works on Windows, we'll need the actually generated files for non Windows developers.

.gitignore
@@ -258,7 +258,3 @@ paket-files/
 # Python Tools for Visual Studio (PTVS)
 __pycache__/
 *.pyc
-
-# Empty Keys Generated
-assets/Gui/
-src/Hero6.UserInterface.SierraVGA/View/*.xaml.cs

assets/Gui/SierraVga/Fonts/Segoe_UI_11.25_Regular.spritefont
@@ -1,0 +1,20 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<XnaContent xmlns:Graphics="Microsoft.Xna.Framework.Content.Pipeline.Graphics">
+  <Asset Type="Graphics:FontDescription">
+    <!-- Value replaced by generator -->
+    <FontName>Segoe UI</FontName>
+    <!-- Value replaced by generator -->
+    <Size>11.25</Size>
+    <Spacing>0</Spacing>
+    <UseKerning>true</UseKerning>
+    <!-- Value replaced by generator -->
+    <Style>Regular</Style>
+    <DefaultCharacter>*</DefaultCharacter>
+    <CharacterRegions>
+      <CharacterRegion>
+        <Start>&#32;</Start>
+        <End>&#383;</End>
+      </CharacterRegion>
+    </CharacterRegions>
+  </Asset>
+</XnaContent>

assets/Hero6.Content.mgcb
@@ -205,13 +205,13 @@
 /processorParam:TextureFormat=Color
 /build:Sprites/Characters/NPCs/Llewella/LlewellaWalkCenterDown.png
 
-#begin Gui/SierraVga/Segoe_UI_11.25_Regular.spritefont
+#begin Gui/SierraVga/Fonts/Segoe_UI_11.25_Regular.spritefont
 /importer:FontDescriptionImporter
 /processor:FontDescriptionProcessor
 /processorParam:TextureFormat=Compressed
-/build:Gui/SierraVga/Segoe_UI_11.25_Regular.spritefont
+/build:Gui/SierraVga/Fonts/Segoe_UI_11.25_Regular.spritefont
 
-#begin Gui/SierraVga/MouseCursors/Arrow.png
+#begin Gui/SierraVga/Cursors/Arrow.png
 /importer:TextureImporter
 /processor:TextureProcessor
 /processorParam:ColorKeyColor=255,0,255,255
@@ -221,9 +221,9 @@
 /processorParam:ResizeToPowerOfTwo=False
 /processorParam:MakeSquare=False
 /processorParam:TextureFormat=Color
-/build:Gui/SierraVga/MouseCursors/Arrow.png
+/build:Gui/SierraVga/Cursors/Arrow.png
 
-#begin Gui/SierraVga/MouseCursors/Wait.png
+#begin Gui/SierraVga/Cursors/Wait.png
 /importer:TextureImporter
 /processor:TextureProcessor
 /processorParam:ColorKeyColor=255,0,255,255
@@ -233,5 +233,5 @@
 /processorParam:ResizeToPowerOfTwo=False
 /processorParam:MakeSquare=False
 /processorParam:TextureFormat=Color
-/build:Gui/SierraVga/MouseCursors/Wait.png
+/build:Gui/SierraVga/Cursors/Wait.png
 

src/Hero6.UserInterface.SierraVGA/Hero6.UserInterface.SierraVGA.csproj
@@ -93,7 +93,7 @@
   <PropertyGroup>
     <PreBuildEvent>CD "$(SolutionDir)packages\EmptyKeysUI_Generator*\tools"
 
-ekUIGen.exe -i="$(ProjectDir)View" -o="$(ProjectDir)View" -oa="$(SolutionDir)assets\Gui\SierraVga" -ns="LateStartStudio.Hero6.UserInterface.SierraVga.View" -rm=MonoGame</PreBuildEvent>
+ekUIGen.exe -i="$(ProjectDir)View" -o="$(ProjectDir)View" -oa="$(SolutionDir)assets\Gui\SierraVga\Fonts" -ns="LateStartStudio.Hero6.UserInterface.SierraVga.View" -rm=MonoGame</PreBuildEvent>
   </PropertyGroup>
   <Choose>
     <When Condition="$(TargetFrameworkIdentifier) == '.NETFramework' And ($(TargetFrameworkVersion) == 'v4.5' Or $(TargetFrameworkVersion) == 'v4.5.1' Or $(TargetFrameworkVersion) == 'v4.5.2' Or $(TargetFrameworkVersion) == 'v4.5.3' Or $(TargetFrameworkVersion) == 'v4.6' Or $(TargetFrameworkVersion) == 'v4.6.1' Or $(TargetFrameworkVersion) == 'v4.6.2' Or $(TargetFrameworkVersion) == 'v4.6.3')">

src/Hero6.UserInterface.SierraVGA/MouseCursor.cs
@@ -87,8 +87,8 @@ namespace LateStartStudio.Hero6.UserInterface.SierraVga
 
         public void Load()
         {
-            this.arrow = this.LoadTexture("MouseCursors/Arrow");
-            this.wait = this.LoadTexture("MouseCursors/Wait");
+            this.arrow = this.LoadTexture("Arrow");
+            this.wait = this.LoadTexture("Wait");
 
             this.Current = this.arrow;
         }
@@ -113,7 +113,9 @@ namespace LateStartStudio.Hero6.UserInterface.SierraVga
 
         private TextureBase LoadTexture(string id)
         {
-            return Engine.Instance.AssetManager.LoadTexture(this.content, id);
+            return Engine.Instance.AssetManager.LoadTexture(
+                this.content,
+                string.Format("Cursors/{0}", id));
         }
     }
 }

src/Hero6.UserInterface.SierraVGA/SierraVGAController.cs
@@ -93,7 +93,9 @@ namespace LateStartStudio.Hero6.UserInterface.SierraVga
 
         private FontBase LoadFont(string id)
         {
-            return UiEngine.Instance.AssetManager.LoadFont(this.Content.NativeContentManager, id);
+            return UiEngine.Instance.AssetManager.LoadFont(
+                this.Content.NativeContentManager,
+                string.Format("Fonts/{0}", id));
         }
     }
 }

src/Hero6.UserInterface.SierraVGA/View/RootView.xaml.cs
@@ -1,0 +1,79 @@
+// -----------------------------------------------------------
+//  
+//  This file was generated, please do not modify.
+//  
+// -----------------------------------------------------------
+namespace LateStartStudio.Hero6.UserInterface.SierraVga.View {
+    using System;
+    using System.CodeDom.Compiler;
+    using System.Collections.ObjectModel;
+    using EmptyKeys.UserInterface;
+    using EmptyKeys.UserInterface.Charts;
+    using EmptyKeys.UserInterface.Data;
+    using EmptyKeys.UserInterface.Controls;
+    using EmptyKeys.UserInterface.Controls.Primitives;
+    using EmptyKeys.UserInterface.Input;
+    using EmptyKeys.UserInterface.Interactions.Core;
+    using EmptyKeys.UserInterface.Interactivity;
+    using EmptyKeys.UserInterface.Media;
+    using EmptyKeys.UserInterface.Media.Effects;
+    using EmptyKeys.UserInterface.Media.Animation;
+    using EmptyKeys.UserInterface.Media.Imaging;
+    using EmptyKeys.UserInterface.Shapes;
+    using EmptyKeys.UserInterface.Renderers;
+    using EmptyKeys.UserInterface.Themes;
+    
+    
+    [GeneratedCodeAttribute("Empty Keys UI Generator", "2.5.0.0")]
+    public partial class RootView : UIRoot {
+        
+        public RootView() : 
+                base() {
+            this.Initialize();
+        }
+        
+        public RootView(int width, int height) : 
+                base(width, height) {
+            this.Initialize();
+        }
+        
+        private void Initialize() {
+            Style style = RootStyle.CreateRootStyle();
+            style.TargetType = this.GetType();
+            this.Style = style;
+            this.InitializeComponent();
+        }
+        
+        private void InitializeComponent() {
+            this.Background = new SolidColorBrush(new ColorW(255, 255, 255, 0));
+            this.FontSize = 15F;
+            Binding binding__OwnedWindowsContent = new Binding("Windows");
+            this.SetBinding(UIRoot.OwnedWindowsContentProperty, binding__OwnedWindowsContent);
+            InitializeElementResources(this);
+            FontManager.Instance.AddFont("Segoe UI", 15F, FontStyle.Regular, "Segoe_UI_11.25_Regular");
+        }
+        
+        private static void InitializeElementResources(UIElement elem) {
+            // Resource - [TextBoxWindow] ControlTemplate
+            Func<UIElement, UIElement> r_0_ctFunc = r_0_ctMethod;
+            ControlTemplate r_0_ct = new ControlTemplate(r_0_ctFunc);
+            elem.Resources.Add("TextBoxWindow", r_0_ct);
+        }
+        
+        private static UIElement r_0_ctMethod(UIElement parent) {
+            // e_0 element
+            Border e_0 = new Border();
+            e_0.Parent = parent;
+            e_0.Name = "e_0";
+            e_0.Background = new SolidColorBrush(new ColorW(0, 0, 0, 255));
+            // e_1 element
+            TextBlock e_1 = new TextBlock();
+            e_0.Child = e_1;
+            e_1.Name = "e_1";
+            e_1.TextWrapping = TextWrapping.Wrap;
+            Binding binding_e_1_Text = new Binding("Text");
+            e_1.SetBinding(TextBlock.TextProperty, binding_e_1_Text);
+            return e_0;
+        }
+    }
+}