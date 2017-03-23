55831fc1c03956fbffbbeaff5f8d9a8a9ceb1b8e
Per Olav Flaten <perolavflaten@live.no>
2017-03-23 13:15:39 +0100

    Added unit test project for AdventureGame module
    Also added some mock objects, a dummy test, and extended the build scripts to include the new test assembly.

---
+++ src/AdventureGame.Tests/DummyTests.cs
@@ -0,0 +1,14 @@
+﻿namespace LateStartStudio.AdventureGame.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void Dummy()
+        {
+            Assert.Pass();
+        }
+    }
+}
