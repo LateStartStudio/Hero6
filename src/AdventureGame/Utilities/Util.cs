c48085694e1c650b6d925d219c2b2f6c9ff38a6d
Per Olav Flaten <perolavflaten@live.no>
2017-03-24 06:55:16 +0100

    Added user settings interface and platform implementations

--- src/AdventureGame/Utilities/Util.cs
+++ src/AdventureGame/Utilities/Util.cs
@@ -11,10 +11,19 @@
 
 namespace LateStartStudio.AdventureGame.Utilities
 {
+    using Settings;
+
     /// <summary>
     /// A collection of utility tools.
     /// </summary>
     public class Util
     {
+        /// <summary>
+        /// Gets or sets the user settings utility.
+        /// </summary>
+        /// <value>
+        /// The user settings utility.
+        /// </value>
+        public static IUserSettings UserSettings { get; set; }
     }
 }
