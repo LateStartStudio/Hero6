158c49289805fbceaf1e97053480c8abb5bb0f9f
Per Olav Flaten <perolavflaten@live.no>
2017-03-27 23:01:09 +0200

    Added unit tests for GameLoop event args

---
+++ src/AdventureGame.Tests/GameLoop/LoadEventArgsTests.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoadEventArgsTests.cs" company="LateStartStudio">
+//   Copyright (C) LateStartStudio
+//   This file is subject to the terms and conditions of the MIT license specified
+//   in the file 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+// <summary>
+//   Defines the LoadEventArgsTests unit tests.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LateStartStudio.AdventureGame.GameLoop
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LoadEventArgsTests
+    {
+        [Test]
+        public void IsNotNull()
+        {
+            Assert.IsNotNull(new LoadEventArgs(MockCampaign.Instance.Content));
+        }
+
+        [Test]
+        public void ContentGet()
+        {
+            Assert.AreEqual(MockCampaign.Instance.Content, new LoadEventArgs(MockCampaign.Instance.Content).Content);
+        }
+    }
+}
