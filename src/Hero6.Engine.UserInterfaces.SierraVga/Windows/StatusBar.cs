ff9e5dd2985c6105419cd6d59d2ec8a30615851b
Per Olav Flaten <perolavflaten@live.no>
2017-05-02 01:05:45 +0200

    Refactored "Top Bar" to "Status Bar" ddd

--- src/Hero6.Engine.UserInterfaces.SierraVga/Windows/StatusBar.cs
+++ src/Hero6.Engine.UserInterfaces.SierraVga/Windows/StatusBar.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="TopBar.cs" company="Late Start Studio">
+﻿// <copyright file="StatusBar.cs" company="Late Start Studio">
 // Copyright (C) Late Start Studio
 // This file is subject to the terms and conditions of the MIT license specified in the file
 // 'LICENSE.CODE.md', which is a part of this source code package.
@@ -11,13 +11,13 @@ namespace LateStartStudio.Hero6.Engine.UserInterfaces.SierraVga.Windows
     using LateStartStudio.Hero6.Engine.Assets;
     using LateStartStudio.Hero6.Engine.UserInterfaces.Controls;
 
-    public class TopBar : Window
+    public class StatusBar : Window
     {
-        public TopBar(AssetManager assets)
+        public StatusBar(AssetManager assets)
             : base(assets)
         {
             this.IsVisible = true;
-            this.Child = new Image(assets, $"Top Bar{Path.DirectorySeparatorChar}Background");
+            this.Child = new Image(assets, $"Status Bar{Path.DirectorySeparatorChar}Background");
         }
     }
 }
