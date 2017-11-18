d64c30c1accb372b345c40ddab532e9089dbf4bb
Per Olav Flaten <perolavflaten@live.no>
2017-11-18 01:23:04 +0100

    Added mock for Texture2D

---
+++ src/Hero6.Engine.Tests/Assets/Graphics/Texture2DMock.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="Texture2DMock.cs" company="Late Start Studio">
+// Copyright (C) Late Start Studio
+// This file is subject to the terms and conditions of the MIT license specified in the file
+// 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+
+namespace LateStartStudio.Hero6.Engine.Assets.Graphics
+{
+    public class Texture2DMock : Texture2D
+    {
+        public Texture2DMock(string id, int width, int height)
+        {
+            this.GetTexture = id;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override int Width { get; }
+
+        public override int Height { get; }
+
+        public override object GetTexture { get; }
+
+        public override void GetData<T>(T[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetData<T>(T[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
