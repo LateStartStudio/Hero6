68c62e1569a1f461ce576d60dbf7d48164038e9b
Per Olav Flaten <perolavflaten@live.no>
2017-11-18 11:17:26 +0100

    Added TestBase for Campaign elements

---
+++ src/Hero6.Engine.Tests/Campaigns/TestBase.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="TestBase.cs" company="Late Start Studio">
+// Copyright (C) Late Start Studio
+// This file is subject to the terms and conditions of the MIT license specified in the file
+// 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+
+namespace LateStartStudio.Hero6.Engine.Campaigns
+{
+    using LateStartStudio.Hero6.Engine.Assets;
+    using NUnit.Framework;
+
+    public class TestBase
+    {
+        protected CampaignMock Campaign { get; private set; }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            RendererMock.IsPaused = false;
+            RendererMock.IsDrawInvoked = false;
+            RendererMock.IsDrawStringInvoked = false;
+            Campaign = CampaignMock.Make();
+            Campaign.Load();
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            Campaign.Unload();
+        }
+    }
+}
