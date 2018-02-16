3afa3743accdee98fc750715c78aebb572542f1f
Per Olav Flaten <perolavflaten@live.no>
2018-02-16 02:11:43 +0100

    Added unit tests for services

---
+++ src/Hero6.Tests/Utilities/DependencyInjection/MonoGameServicesTests.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="MonoGameServicesTests.cs" company="Late Start Studio">
+// Copyright (C) Late Start Studio
+// This file is subject to the terms and conditions of the MIT license specified in the file
+// 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+
+namespace LateStartStudio.Hero6.Utilities.DependencyInjection
+{
+    using Engine.Utilities.DependencyInjection;
+    using Microsoft.Xna.Framework;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MonoGameServicesTests : ServicesTests
+    {
+        protected override IServices Make() => new MonoGameServices(new GameServiceContainer());
+    }
+}
