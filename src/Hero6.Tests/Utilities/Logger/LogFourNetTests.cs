6ed3f658d508fa164cf3ae80d42fcdc617e3451c
Per Olav Flaten <perolavflaten@live.no>
2018-02-16 01:52:27 +0100

    Added unit tests for LogFourNet

---
+++ src/Hero6.Tests/Utilities/Logger/LogFourNetTests.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="LogFourNetTests.cs" company="Late Start Studio">
+// Copyright (C) Late Start Studio
+// This file is subject to the terms and conditions of the MIT license specified in the file
+// 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+
+namespace LateStartStudio.Hero6.Engine.Utilities.Logger
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class LogFourNetTests : LoggerTests
+    {
+        protected override ILogger Make() => new LogFourNet(Filename);
+    }
+}
