87077dd3f3188ea1272cd7caf9b707fde66c74c8
Per Olav Flaten <perolavflaten@live.no>
2018-12-08 02:39:52 +0100

    Replaced GUI crash log with opening text file, removed 3rd party framew.

--- src/Hero6/Engine/Utilities/Logger/LogFourNet.cs
+++ src/Hero6/Engine/Utilities/Logger/LogFourNet.cs
@@ -9,9 +9,9 @@ namespace LateStartStudio.Hero6.Engine.Utilities.Logger
     using System;
     using System.IO;
     using System.Reflection;
+    using LateStartStudio.Hero6.Engine.Utilities.Settings;
     using log4net;
     using log4net.Config;
-    using Settings;
 
     /// <summary>
     /// The LogFourNet class, wrapper around the popular framework log4net.
@@ -27,11 +27,18 @@ namespace LateStartStudio.Hero6.Engine.Utilities.Logger
         public LogFourNet(IUserSettings userSettings)
         {
             this.userSettings = userSettings;
-            this.Filename = LogFileName;
-            this.WillDeleteLogOnDispose = true;
-            this.log = LogManager.GetLogger(typeof(LogFourNet));
+            Filename = LogFileName;
+            WillDeleteLogOnDispose = true;
+            log = LogManager.GetLogger(typeof(LogFourNet));
             var repo = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(repo, new FileInfo("Hero6.Logger.config"));
+
+            Info("This is the Hero6 log, if you're seeing this file it's most likely because Hero6 has crashed. Provide this file when you report the crash.");
+            Info($"Hero6 {Game.GraphicsApi} v{Assembly.GetExecutingAssembly().GetName().Version} Log");
+            Info("Forums: http://www.hero6.org/forum/");
+            Info("Bug Tracker: https://github.com/LateStartStudio/Hero6/issues");
+            Info("E-mail: hero6lives@gmail.com");
+            Info("Creating Hero6 Game Instance.");
         }
 
         /// <summary>
@@ -40,7 +47,7 @@ namespace LateStartStudio.Hero6.Engine.Utilities.Logger
         /// </summary>
         ~LogFourNet()
         {
-            this.Dispose();
+            Dispose();
         }
 
         /// <inheritdoc />
@@ -60,100 +67,28 @@ namespace LateStartStudio.Hero6.Engine.Utilities.Logger
         /// <inheritdoc />
         public void Dispose()
         {
-            if (this.WillDeleteLogOnDispose)
+            if (WillDeleteLogOnDispose)
             {
-                File.Delete(this.Filename);
+                File.Delete(Filename);
             }
         }
 
         /// <inheritdoc />
-        public void Debug(string message)
-        {
-            if (this.log.IsDebugEnabled)
-            {
-                this.log.Debug(message);
-            }
-        }
+        public void Info(string message) => log.Info(message);
 
         /// <inheritdoc />
-        public void Debug(string message, Exception e)
-        {
-            if (this.log.IsDebugEnabled)
-            {
-                this.log.Debug(message, e);
-            }
-        }
+        public void Info(string message, Exception e) => log.Info(message, e);
 
         /// <inheritdoc />
-        public void Info(string message)
-        {
-            if (this.log.IsInfoEnabled)
-            {
-                this.log.Info(message);
-            }
-        }
+        public void Warning(string message) => log.Warn(message);
 
         /// <inheritdoc />
-        public void Info(string message, Exception e)
-        {
-            if (this.log.IsInfoEnabled)
-            {
-                this.log.Info(message, e);
-            }
-        }
+        public void Warning(string message, Exception e) => log.Warn(message, e);
 
         /// <inheritdoc />
-        public void Warning(string message)
-        {
-            if (this.log.IsWarnEnabled)
-            {
-                this.log.Warn(message);
-            }
-        }
+        public void Error(string message) => log.Error(message);
 
         /// <inheritdoc />
-        public void Warning(string message, Exception e)
-        {
-            if (this.log.IsWarnEnabled)
-            {
-                this.log.Warn(message, e);
-            }
-        }
-
-        /// <inheritdoc />
-        public void Error(string message)
-        {
-            if (this.log.IsErrorEnabled)
-            {
-                this.log.Error(message);
-            }
-        }
-
-        /// <inheritdoc />
-        public void Error(string message, Exception e)
-        {
-            if (this.log.IsErrorEnabled)
-            {
-                this.log.Error(message, e);
-            }
-        }
-
-        /// <inheritdoc />
-        public void Fatal(string message)
-        {
-            if (this.log.IsFatalEnabled)
-            {
-                this.log.Fatal(message);
-            }
-        }
-
-        /// <inheritdoc />
-        public void Fatal(string message, Exception e)
-        {
-            if (this.log.IsFatalEnabled)
-            {
-                this.log.Fatal(message, e);
-            }
-        }
+        public void Error(string message, Exception e) => log.Error(message, e);
     }
 }
