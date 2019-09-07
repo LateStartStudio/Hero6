c489a62660acdf49bec3999517a691fc82c72c38
Per Olav Flaten <perolavflaten@live.no>
2019-09-07 15:21:03 +0200

    Test .net build on mono

--- scripts/build-monogame-pipeline.sh
+++ scripts/build-monogame-pipeline.sh
@@ -5,9 +5,11 @@ if [ -x $CONFIG ]; then
   exit 1
 fi
 
-if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-  dotnet build $(dirname $0)/../src/Hero6.MonoGamePipeline.sln -c $CONFIG
-else
-  msbuild $(dirname $0)/../src/Hero6.MonoGamePipeline.sln /t:restore /property:Configuration=$CONFIG
-  msbuild $(dirname $0)/../src/Hero6.MonoGamePipeline.sln /property:Configuration=$CONFIG
-fi
+dotnet build $(dirname $0)/../src/Hero6.MonoGamePipeline.sln -c $CONFIG
+
+# if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+#   dotnet build $(dirname $0)/../src/Hero6.MonoGamePipeline.sln -c $CONFIG
+# else
+#   msbuild $(dirname $0)/../src/Hero6.MonoGamePipeline.sln /t:restore /property:Configuration=$CONFIG
+#   msbuild $(dirname $0)/../src/Hero6.MonoGamePipeline.sln /property:Configuration=$CONFIG
+# fi
