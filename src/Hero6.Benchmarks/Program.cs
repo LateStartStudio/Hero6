60883ff0cfbc436f6f46bb9aa6464c784c2fc7f0
Per Olav Flaten <perolavflaten@live.no>
2019-10-04 20:56:59 +0200

    Added projects for Benchmarking

---
+++ src/Hero6.Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="Program.cs" company="Late Start Studio">
+// Copyright (C) Late Start Studio
+// This file is subject to the terms and conditions of the MIT license specified in the file
+// 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+
+using System;
+
+namespace Hero6.Desktop.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
