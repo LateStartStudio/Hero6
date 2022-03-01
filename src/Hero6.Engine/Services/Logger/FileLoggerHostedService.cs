b883cf882689b256a4822de436d59bf1e173f81d
Per Olav Flaten <perolavflaten@live.no>
2022-03-02 00:56:59 +0100

    Replaced log4net with NLog
    log4net doesn't seem to updating their framework anymore. Since we have to update our logging to be compatible with Microsoft Host anyway it makes sense to change with something that receives regular updates

---
+++ src/Hero6.Engine/Services/Logger/FileLoggerHostedService.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="FileLoggerHostedService.cs" company="Late Start Studio">
+// Copyright (C) Late Start Studio
+// This file is subject to the terms and conditions of the MIT license specified in the file
+// 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using LateStartStudio.Hero6.Services.PlatformInfo;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Hero6.Engine.Services.Logger
+{
+    /// <summary>
+    /// Handles Hero6 file logs.
+    /// </summary>
+    public class FileLoggerHostedService : IHostedService
+    {
+        private readonly ILogger logger;
+        private readonly IPlatformInfo platformInfo;
+        private readonly FileLoggerSettings fileLoggerSettings;
+
+        public FileLoggerHostedService(ILogger<FileLoggerHostedService> logger, IPlatformInfo platformInfo, FileLoggerSettings fileLoggerSettings)
+        {
+            this.logger = logger;
+            this.platformInfo = platformInfo;
+            this.fileLoggerSettings = fileLoggerSettings;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("This is the Hero6 log, if you're seeing this file it's most likely because Hero6 has crashed. Provide this file when you report the crash.");
+            logger.LogInformation($"Hero6 {platformInfo.Platform} v{Assembly.GetExecutingAssembly().GetName().Version} Log");
+            logger.LogInformation("Forums: http://www.hero6.org/forum/");
+            logger.LogInformation("Bug Tracker: https://github.com/LateStartStudio/Hero6/issues");
+            logger.LogInformation("E-mail: hero6lives@gmail.com");
+            logger.LogInformation("Creating Hero6 Game Instance.");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (fileLoggerSettings.WillDeleteLogOnDispose)
+            {
+                File.Delete(fileLoggerSettings.FileName);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Hero6.Engine/Services/Logger/FileLoggerSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Linq;
+using LateStartStudio.Hero6.Services.Settings;
+
+namespace Hero6.Engine.Services.Logger
+{
+    /// <summary>
+    /// Microsoft.Extensions.Logging doesn't support file logging out of the box so we need a 3rd party
+    /// framework and also to configure it.
+    /// </summary>
+    public class FileLoggerSettings
+    {
+        private readonly IGameSettings gameSettings;
+
+        public FileLoggerSettings(IGameSettings gameSettings, IUserSettings userSettings)
+        {
+            this.gameSettings = gameSettings;
+
+            const string logFileNameFormat = "{0}Hero6-Log-{1}.txt";
+            var logFileName = Enumerable
+                .Range(0, int.MaxValue)
+                .Select(i => string.Format(logFileNameFormat, LogFilesDir, i))
+                .First(f => !File.Exists(f));
+            FileName = logFileName;
+        }
+
+        public string FileName { get; }
+
+        public bool WillDeleteLogOnDispose { get; set; } = true;
+
+        private string LogFilesDir => $"{gameSettings.UserFilesDir}.logs{Path.DirectorySeparatorChar}";
+    }
+}
