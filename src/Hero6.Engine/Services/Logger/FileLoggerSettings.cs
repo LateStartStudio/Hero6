2ef49fa9c6313bfa1cb9d375994239d37d165919
Per Olav Flaten <perolavflaten@live.no>
2022-03-02 01:05:01 +0100

    Update logger file header

--- src/Hero6.Engine/Services/Logger/FileLoggerSettings.cs
+++ src/Hero6.Engine/Services/Logger/FileLoggerSettings.cs
@@ -1,4 +1,10 @@
-﻿using System.IO;
+﻿// <copyright file="FileLoggerSettings.cs" company="Late Start Studio">
+// Copyright (C) Late Start Studio
+// This file is subject to the terms and conditions of the MIT license specified in the file
+// 'LICENSE.CODE.md', which is a part of this source code package.
+// </copyright>
+
+using System.IO;
 using System.Linq;
 using LateStartStudio.Hero6.Services.Settings;
 
